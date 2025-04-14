6bf7b42ed008aaa75fc313f9ad2fbb32184e4df2
matveev96 <sasha.matveev1996@icloud.com>
2025-04-14 21:02:22 +0300

    feat: add CustomLinks

--- src/widgets/dashboard/ui/NavigateBlock.tsx
+++ src/widgets/dashboard/ui/NavigateBlock.tsx
@@ -1,12 +1,16 @@
-import { Link } from '../../../shared/ui/links/Link.tsx'
+import { CustomLink } from '../../../shared/ui/links/CustomLink.tsx'
 import { PATH } from '../../../app/router/router.tsx'
+import { Box } from '@mui/material'
+import Company from '../../../shared/assets/icons/common/Company.svg'
+import Account from '../../../shared/assets/icons/common/Account.svg'
+import Contractor from '../../../shared/assets/icons/common/Contractor.svg'
 
 export const NavigateBlock = () => {
   return (
-    <div>
-      <Link name={'Organizations'} path={PATH.Organizations} />
-      <Link name={'Contractors'} path={PATH.Contractors} />
-      <Link name={'Clients'} path={PATH.Clients} />
-    </div>
+    <Box sx={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
+      <CustomLink name={'Organizations'} path={PATH.Organizations} icon={Company} />
+      <CustomLink name={'Contractors'} path={PATH.Contractors} icon={Contractor} />
+      <CustomLink name={'Clients'} path={PATH.Clients} icon={Account} />
+    </Box>
   )
 }
