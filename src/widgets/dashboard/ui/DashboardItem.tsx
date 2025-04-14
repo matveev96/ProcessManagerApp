6c05a30fe6a6bd669a6c31348c4679a42008e8cb
matveev96 <sasha.matveev1996@icloud.com>
2025-04-14 21:02:32 +0300

    feat: add Typography

--- src/widgets/dashboard/ui/DashboardItem.tsx
+++ src/widgets/dashboard/ui/DashboardItem.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { Border } from '../../../shared/ui/border/Border.tsx'
 import { TypographyBlock } from './TypographyBlock.tsx'
 import { NavigateBlock } from './NavigateBlock.tsx'
@@ -13,11 +13,28 @@ export const DashboardItem = () => {
         left: 48,
         boxShadow: '4px 0 16px 0 rgba(0, 0, 0, 0.08)',
         padding: '20px',
+        backgroundColor: '#fff',
       }}
     >
       <TypographyBlock />
       <Border color={'rgba(0, 0, 0, 0.1)'} width={'100%'} />
       <NavigateBlock />
+      <Typography
+        variant="h3"
+        component="div"
+        sx={{
+          fontSize: '11px',
+          fontWeight: 400,
+          lineHeight: '1.4',
+          letterSpacing: '0.02em',
+          color: 'rgba(0, 0, 0, 0.3)',
+          position: 'absolute',
+          left: 30,
+          bottom: 20,
+        }}
+      >
+        All Funeral Services © 2015-2025
+      </Typography>
     </Box>
   )
 }
