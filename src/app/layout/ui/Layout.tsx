60359d7bb74ab28689f03f636f4554cf0c35c22b
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:47:54 +0300

    feat: add Login to Layout

--- src/app/layout/ui/Layout.tsx
+++ src/app/layout/ui/Layout.tsx
@@ -1,14 +1,16 @@
 import { SidebarItem } from '../../../widgets/sidebar/ui/SidebarItem.tsx'
 import { DashboardItem } from '../../../widgets/dashboard/ui/DashboardItem.tsx'
 import { Content } from '../../../widgets/content/Content.tsx'
+import { Login } from '../../../pages/login/Login.tsx'
 
 const Layout = () => {
   return (
-    <div>
+    <>
       <SidebarItem />
       <DashboardItem />
       <Content />
-    </div>
+      <Login />
+    </>
   )
 }
 
