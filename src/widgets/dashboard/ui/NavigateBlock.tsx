c9fcbf7d847409676095671d353633e8c563189c
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:44:24 +0300

    feat: add setActive

--- src/widgets/dashboard/ui/NavigateBlock.tsx
+++ src/widgets/dashboard/ui/NavigateBlock.tsx
@@ -4,13 +4,35 @@ import { Box } from '@mui/material'
 import Company from '../../../shared/assets/icons/common/Company.svg'
 import Account from '../../../shared/assets/icons/common/Account.svg'
 import Contractor from '../../../shared/assets/icons/common/Contractor.svg'
+import s from '../../../shared/ui/links/link.module.scss'
+import { useState } from 'react'
 
 export const NavigateBlock = () => {
+  const [active, setActive] = useState({ name: 'Organizations' })
+
   return (
     <Box sx={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
-      <CustomLink name={'Organizations'} path={PATH.Organizations} icon={Company} />
-      <CustomLink name={'Contractors'} path={PATH.Contractors} icon={Contractor} />
-      <CustomLink name={'Clients'} path={PATH.Clients} icon={Account} />
+      <CustomLink
+        onClick={() => setActive({ name: 'Organizations' })}
+        className={`${active.name === 'Organizations' ? s.activeLinkButton : ''}`}
+        name={'Organizations'}
+        path={PATH.Organizations}
+        icon={Company}
+      />
+      <CustomLink
+        onClick={() => setActive({ name: 'Contractors' })}
+        className={`${active.name === 'Contractors' ? s.activeLinkButton : ''}`}
+        name={'Contractors'}
+        path={PATH.Contractors}
+        icon={Contractor}
+      />
+      <CustomLink
+        onClick={() => setActive({ name: 'Clients' })}
+        className={`${active.name === 'Clients' ? s.activeLinkButton : ''}`}
+        name={'Clients'}
+        path={PATH.Clients}
+        icon={Account}
+      />
     </Box>
   )
 }
