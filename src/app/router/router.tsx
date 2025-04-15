15e84da32a0d5d2f4f3c5c36e94fa39a9cbc3e69
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:47:36 +0300

    feat: fix router

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,19 +1,13 @@
 import { createBrowserRouter, Navigate, type RouteObject } from 'react-router-dom'
 import App from '../App'
-// import { Error404 } from '../components/pages/Error404'
 import { Organizations } from '../../pages/organizations/Organizations.tsx'
 import { Contractors } from '../../pages/contractors/Contractors.tsx'
 import { Clients } from '../../pages/clients/Clients.tsx'
-// import { Model } from '../components/pages/Model'
-// import Login from '../components/pages/Login'
 
 export const PATH = {
   Organizations: '/organizations',
   Contractors: '/contractors',
   Clients: '/clients',
-  MODEL: '/:model/:id',
-  ERROR: '/error',
-  LOGIN: '/login',
   MAIN_PAGE: '/',
 } as const
 
@@ -30,18 +24,6 @@ const publicRoutes: RouteObject[] = [
     path: PATH.Clients,
     element: <Clients />,
   },
-  // {
-  //   path: PATH.LOGIN,
-  //   element: <Login />,
-  // },
-  // {
-  //   path: PATH.MODEL,
-  //   element: <Model />,
-  // },
-  // {
-  //   path: PATH.ERROR,
-  //   element: <Error404 />,
-  // },
   {
     path: PATH.MAIN_PAGE,
     element: <Navigate to={PATH.Organizations} />,
