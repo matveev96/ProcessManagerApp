5666f846580775fcc6b009cc73df2673e8524de0
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:45:53 +0300

    feat: update CustomIconButton

--- src/shared/ui/buttons/CustomIconButton.tsx
+++ src/shared/ui/buttons/CustomIconButton.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@mui/material'
 
 type Props = {
-  icon: string
-  ariaLabel: string
+  icon?: string
+  ariaLabel?: string
+  onClick?: () => void
+  variant: 'text' | 'outlined' | 'contained'
 }
 
 export const CustomIconButton = (props: Props) => {
-  const { icon, ariaLabel } = props
+  const { icon, ariaLabel, onClick, variant } = props
 
   return (
-    <Button color="secondary" aria-label={ariaLabel} variant={'text'} sx={{ borderRadius: '20px' }}>
+    <Button onClick={onClick} color="secondary" aria-label={ariaLabel} variant={variant} sx={{ borderRadius: '20px' }}>
       <img src={icon} alt={ariaLabel} />
     </Button>
   )
