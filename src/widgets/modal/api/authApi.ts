d85f6171e2c67f55ca0e87daa82b9d9de3750c8b
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:43:03 +0300

    feat: implement authApi

---
+++ src/widgets/modal/api/authApi.ts
@@ -0,0 +1,24 @@
+import { baseApi } from '../../../app/store/baseApi.ts'
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    login: build.mutation<any, { username: string }>({
+      query: (username) => `auth?user=${username}`,
+      async onQueryStarted(arg, { queryFulfilled }) {
+        try {
+          const { meta } = await queryFulfilled
+          const headers = meta?.response?.headers
+          const token = headers?.get('Authorization')
+
+          if (token) {
+            localStorage.setItem('token', token)
+          }
+        } catch (error) {
+          console.error('Ошибка запроса', error)
+        }
+      },
+    }),
+  }),
+})
+
+export const { useLoginMutation } = authApi
