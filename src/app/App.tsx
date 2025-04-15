6d9d7803b62baf8628cb34406edf6dc868a52a66
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:46:35 +0300

    feat: add isLoggedIn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,21 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { appTheme } from '../shared/lib/theme.ts'
 import Layout from './layout/ui/Layout.tsx'
+import { Login } from '../pages/login/Login.tsx'
+import { selectIsLoggedIn, setModalLogin } from './store/appSlice.ts'
+import { useAppSelector } from './store/hooks/useAppSelector.ts'
+import { useAppDispatch } from './store/hooks/useAppDispatch.ts'
 
 function App() {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+  const dispatch = useAppDispatch()
+  if (!isLoggedIn) {
+    dispatch(setModalLogin({ modalLogin: true }))
+  }
   return (
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
-      <Layout />
+      {isLoggedIn ? <Layout /> : <Login />}
     </ThemeProvider>
   )
 }
