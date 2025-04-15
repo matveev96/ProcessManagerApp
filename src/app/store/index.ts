b4ab5166ed9e2a75173c2a9689bfca86395c64ba
matveev96 <sasha.matveev1996@icloud.com>
2025-04-15 18:46:53 +0300

    feat: add RTK store

---
+++ src/app/store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { appReducer, appSlice } from './appSlice.ts'
+import { baseApi } from './baseApi.ts'
+
+export const store = configureStore({
+  reducer: {
+    [appSlice.name]: appReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+})
+setupListeners(store.dispatch)
+
+// определить автоматически тип всего объекта состояния
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+// window.store = store
