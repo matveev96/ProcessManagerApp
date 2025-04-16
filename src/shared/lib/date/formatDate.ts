a5733fd35a43f2a5d310f8681facca9e74574105
matveev96 <sasha.matveev1996@icloud.com>
2025-04-16 22:52:04 +0300

    feat: implement formatDate

---
+++ src/shared/lib/date/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (input: string | undefined) => {
+  const formatInput = input ?? 'yyyy-mm-dd'
+  //Можно с использованием объекта Date или при помощи метода split
+  // const date = new Date(formatInput)
+  // const day = String(date.getDate()).padStart(2, '0')
+  // const month = String(date.getMonth() + 1).padStart(2, '0')
+  // const year = date.getFullYear()
+  const [year, month, day] = formatInput.split('-')
+  return `${day}.${month}.${year}`
+}
