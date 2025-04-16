b4cc785b35f5fc336e0d0d58957862f68a55103c
matveev96 <sasha.matveev1996@icloud.com>
2025-04-16 22:49:44 +0300

    feat: add content to Card

--- src/shared/ui/cards/Card.tsx
+++ src/shared/ui/cards/Card.tsx
@@ -1,9 +1,34 @@
 import { Paper } from '@mui/material'
+import s from './Cards.module.scss'
+import { CustomButton } from '../buttons/CustomButton.tsx'
+import type { ReactNode } from 'react'
 
-export const Card = () => {
+export type CardProps = {
+  title: string
+  buttonIcon: string
+  buttonText: string
+  children?: ReactNode
+}
+
+export const Card = (props: CardProps) => {
+  const { title, buttonIcon, buttonText, children } = props
+
   return (
     <Paper elevation={2} sx={{ borderRadius: '16px' }}>
-      <h2>lorem lorem</h2>
+      <div className={s.cardContent}>
+        <div className={s.cardHeader}>
+          <h3 className={s.cardHeaderTitle}>{title}</h3>
+          <CustomButton
+            color={'primary'}
+            variant={'outlined'}
+            icon={buttonIcon}
+            text={buttonText}
+            ariaLabel={`${buttonIcon}`}
+          />
+        </div>
+
+        <div className={s.cardBody}>{children}</div>
+      </div>
     </Paper>
   )
 }
