4b35c977eeffb6863892d0f52b7ca0c5cc5fb4df
matveev96 <sasha.matveev1996@icloud.com>
2025-04-16 09:08:05 +0300

    feat: add Cards to Organizations

--- src/pages/organizations/Organizations.tsx
+++ src/pages/organizations/Organizations.tsx
@@ -1,3 +1,12 @@
+import { Card } from '../../shared/ui/cards/Card.tsx'
+import { Box } from '@mui/material'
+
 export const Organizations = () => {
-  return <div>Organizations</div>
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
+      <Card />
+      <Card />
+      <Card />
+    </Box>
+  )
 }
