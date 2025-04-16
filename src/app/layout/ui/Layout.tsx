514ef8b57f165f13d8426f13c0dade49636d27d8
matveev96 <sasha.matveev1996@icloud.com>
2025-04-16 09:09:01 +0300

    feat: add Box container

--- src/app/layout/ui/Layout.tsx
+++ src/app/layout/ui/Layout.tsx
@@ -2,15 +2,16 @@ import { SidebarItem } from '../../../widgets/sidebar/ui/SidebarItem.tsx'
 import { DashboardItem } from '../../../widgets/dashboard/ui/DashboardItem.tsx'
 import { Content } from '../../../widgets/content/Content.tsx'
 import { Login } from '../../../pages/login/Login.tsx'
+import { Box } from '@mui/material'
 
 const Layout = () => {
   return (
-    <>
+    <Box sx={{ display: 'flex' }}>
       <SidebarItem />
       <DashboardItem />
       <Content />
       <Login />
-    </>
+    </Box>
   )
 }
 
