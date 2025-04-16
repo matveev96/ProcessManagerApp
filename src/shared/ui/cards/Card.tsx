12a6b167d2704eb31b8ae4769ce8f8dd03357c08
matveev96 <sasha.matveev1996@icloud.com>
2025-04-16 09:07:50 +0300

    feat: realize Card

---
+++ src/shared/ui/cards/Card.tsx
@@ -0,0 +1,9 @@
+import { Paper } from '@mui/material'
+
+export const Card = () => {
+  return (
+    <Paper elevation={2} sx={{ borderRadius: '16px' }}>
+      <h2>lorem lorem</h2>
+    </Paper>
+  )
+}
