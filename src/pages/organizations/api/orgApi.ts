380da681d4cf8a13186521b5d360892977ddea86
matveev96 <sasha.matveev1996@icloud.com>
2025-04-17 13:15:22 +0300

    feat: add getContacts endpoint

--- src/pages/organizations/api/orgApi.ts
+++ src/pages/organizations/api/orgApi.ts
@@ -1,12 +1,15 @@
 import { baseApi } from '../../../app/store/baseApi.ts'
-import type { OrgResponse } from './type.ts'
+import type { CompaniesResponse, ContactsResponse } from './type.ts'
 
 export const orgApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
-    getCompanies: build.query<OrgResponse, { id: string }>({
+    getCompanies: build.query<CompaniesResponse, { id: string }>({
       query: ({ id }) => `companies/${id}`,
+    }),
+    getContacts: build.query<ContactsResponse, { id: string }>({
+      query: ({ id }) => `contacts/${id}`,
     }),
   }),
 })
 
-export const { useGetCompaniesQuery } = orgApi
+export const { useGetCompaniesQuery, useGetContactsQuery } = orgApi
