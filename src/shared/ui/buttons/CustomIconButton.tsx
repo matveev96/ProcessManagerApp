0a46326028fc442ee46a19e51b7d03abb91c9f81
matveev96 <sasha.matveev1996@icloud.com>
2025-04-17 13:12:43 +0300

    refactor: add backgroundColor prop

--- src/shared/ui/buttons/CustomIconButton.tsx
+++ src/shared/ui/buttons/CustomIconButton.tsx
@@ -5,13 +5,20 @@ type Props = {
   ariaLabel?: string
   onClick?: () => void
   color: 'primary' | 'secondary'
+  backgroundColor?: string
 }
 
 export const CustomIconButton = (props: Props) => {
-  const { icon, ariaLabel, onClick, color } = props
+  const { icon, ariaLabel, onClick, color, backgroundColor } = props
 
   return (
-    <IconButton onClick={onClick} color={color} aria-label={ariaLabel} sx={{ borderRadius: '10px' }}>
+    <IconButton
+      size={'small'}
+      onClick={onClick}
+      color={color}
+      aria-label={ariaLabel}
+      sx={{ borderRadius: '10px', backgroundColor }}
+    >
       <img src={icon} alt={ariaLabel} />
     </IconButton>
   )
