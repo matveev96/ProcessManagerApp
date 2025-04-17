efdd2f9e7ba5c67e390901afc2c4b3aa2da778f0
matveev96 <sasha.matveev1996@icloud.com>
2025-04-17 13:14:35 +0300

    feat: realize Contacts Card and Photos Card

--- src/pages/organizations/Organizations.tsx
+++ src/pages/organizations/Organizations.tsx
@@ -3,18 +3,21 @@ import { Box } from '@mui/material'
 import { useGetCompaniesQuery } from './api/orgApi.ts'
 import { CustomIconButton } from '../../shared/ui/buttons/CustomIconButton.tsx'
 import edit from '../../shared/assets/icons/common/Edit.svg'
+import add from '../../shared/assets/icons/common/Add Photo.svg'
 import trash from '../../shared/assets/icons/common/Trash Red.svg'
 import s from './Organizations.module.scss'
-import { formatDate } from '../../shared/lib/date/formatDate.ts'
 import { CardsList } from '../../shared/ui/cards/cardsList/CardsList.tsx'
+import { AgreementLI } from '../../shared/ui/cards/companyBlock/AgreementLI.tsx'
+import { BusinessLI } from '../../shared/ui/cards/companyBlock/BusinessLI.tsx'
+import { CompanyLI } from '../../shared/ui/cards/companyBlock/CompanyLI.tsx'
+import { ResponsibleLI } from '../../shared/ui/cards/contactsBlock/ResponsibleLI.tsx'
+import { PhoneLI } from '../../shared/ui/cards/contactsBlock/PhoneLI.tsx'
+import { EmailLI } from '../../shared/ui/cards/contactsBlock/EmailLI.tsx'
+import { PhotosLi } from '../../shared/ui/cards/photosBlock/PhotosLI.tsx'
 
 export const Organizations = () => {
   const { data } = useGetCompaniesQuery({ id: '12' })
-  const date = formatDate(data?.contract.issue_date.slice(0, 10))
   const header = data?.name
-  const number = data?.contract.no ?? ''
-  const buisnessEntity = data?.businessEntity ?? ''
-  const companiesType = data?.type ?? []
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
@@ -26,10 +29,24 @@ export const Organizations = () => {
         </div>
       </div>
       <Card title={'Company Details'} buttonIcon={edit} buttonText={'Edit'}>
-        <CardsList buisnessEntity={buisnessEntity} companiesType={companiesType} date={date} number={number} />
+        <CardsList>
+          <AgreementLI />
+          <BusinessLI />
+          <CompanyLI />
+        </CardsList>
       </Card>
-      {/*<Card />*/}
-      {/*<Card />*/}
+      <Card title={'Contacts'} buttonIcon={edit} buttonText={'Edit'}>
+        <CardsList>
+          <ResponsibleLI />
+          <PhoneLI />
+          <EmailLI />
+        </CardsList>
+      </Card>
+      <Card title={'Photos'} buttonIcon={add} buttonText={'Add'}>
+        <CardsList>
+          <PhotosLi />
+        </CardsList>
+      </Card>
     </Box>
   )
 }
