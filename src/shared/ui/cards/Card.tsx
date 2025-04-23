d6a0769d4a4ccfa9b036ba46ac053fc1e6912b08
matveev96 <sasha.matveev1996@icloud.com>
2025-04-23 15:32:45 +0300

    refactor: change Card structure

--- src/shared/ui/cards/Card.tsx
+++ src/shared/ui/cards/Card.tsx
@@ -1,27 +1,11 @@
 import { Paper } from '@mui/material'
 import s from './Cards.module.scss'
-import { CustomButton } from '../buttons/CustomButton.tsx'
-import type { CardProps } from './type.ts'
+import type { ReactNode } from 'react'
 
-export const Card = (props: CardProps) => {
-  const { title, buttonIcon, buttonText, children } = props
-
+export const Card = (props: { children: ReactNode }) => {
   return (
     <Paper elevation={2} sx={{ borderRadius: '16px' }}>
-      <div className={s.cardContent}>
-        <div className={s.cardHeader}>
-          <h3 className={s.cardHeaderTitle}>{title}</h3>
-          <CustomButton
-            color={'primary'}
-            variant={'outlined'}
-            icon={buttonIcon}
-            text={buttonText}
-            ariaLabel={`${buttonIcon}`}
-          />
-        </div>
-
-        <div className={s.cardBody}>{children}</div>
-      </div>
+      <div className={s.cardContent}>{props.children}</div>
     </Paper>
   )
 }
