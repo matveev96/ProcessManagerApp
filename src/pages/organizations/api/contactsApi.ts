618f5017eb0793d59f755c538a62131aa157d0ea
matveev96 <sasha.matveev1996@icloud.com>
2025-05-17 17:11:05 +0300

    feat: add companiesApi

---
+++ src/pages/organizations/api/contactsApi.ts
@@ -0,0 +1,12 @@
+import { baseApi } from '../../../app/store/baseApi.ts'
+import type { ContactsResponse } from './type.ts'
+
+export const companiesApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getContacts: build.query<ContactsResponse, { id: string }>({
+      query: ({ id }) => `contacts/${id}`,
+    }),
+  }),
+})
+
+export const { useGetContactsQuery } = companiesApi
