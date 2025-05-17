f25fdfbb311e19c7699624ecd7e4d03b0ecb94c6
matveev96 <sasha.matveev1996@icloud.com>
2025-05-17 17:07:51 +0300

    feat: realize CardsList

--- src/shared/ui/cards/cardsList/CardsList.tsx
+++
@@ -1,7 +0,0 @@
-import s from '../Cards.module.scss'
-import type { CardsListType } from './type.ts'
-
-export const CardsList = (props: CardsListType) => {
-  const { children } = props
-  return <ul className={s.list}>{children}</ul>
-}
