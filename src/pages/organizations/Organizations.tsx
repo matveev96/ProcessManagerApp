bb10b44c2c2b0ae5efb26f4e8698a5558778651a
matveev96 <sasha.matveev1996@icloud.com>
2025-05-17 17:10:22 +0300

    refactor: refactor Organizations

--- src/pages/organizations/Organizations.tsx
+++ src/pages/organizations/Organizations.tsx
@@ -1,12 +1,11 @@
 import { Card } from '../../shared/ui/cards/Card.tsx'
 import { Box } from '@mui/material'
-import { useGetCompaniesQuery } from './api/orgApi.ts'
+import { useGetCompaniesQuery, useUpdateCompaniesMutation } from './api/companiesApi.ts'
 import { CustomIconButton } from '../../shared/ui/buttons/CustomIconButton.tsx'
 import edit from '../../shared/assets/icons/common/Edit.svg'
-import add from '../../shared/assets/icons/common/Add Photo.svg'
 import trash from '../../shared/assets/icons/common/Trash Red.svg'
 import s from './Organizations.module.scss'
-import { CardsList } from '../../shared/ui/cards/cardsList/CardsList.tsx'
+import { CardsBody } from '../../shared/ui/cards/cardsList/CardsBody.tsx'
 import { AgreementLI } from '../../shared/ui/cards/companyBlock/AgreementLI.tsx'
 import { BusinessLI } from '../../shared/ui/cards/companyBlock/BusinessLI.tsx'
 import { CompanyLI } from '../../shared/ui/cards/companyBlock/CompanyLI.tsx'
@@ -14,10 +13,64 @@ import { ResponsibleLI } from '../../shared/ui/cards/contactsBlock/ResponsibleLI
 import { PhoneLI } from '../../shared/ui/cards/contactsBlock/PhoneLI.tsx'
 import { EmailLI } from '../../shared/ui/cards/contactsBlock/EmailLI.tsx'
 import { PhotosLi } from '../../shared/ui/cards/photosBlock/PhotosLI.tsx'
+import { HeaderCompanyBlock } from '../../shared/ui/cards/companyBlock/HeaderCompanyBlock.tsx'
+import { HeaderContactsBlock } from '../../shared/ui/cards/contactsBlock/HeaderContactsBlock.tsx'
+import { HeaderPhotosBlock } from '../../shared/ui/cards/photosBlock/HeaderPhotosBlock.tsx'
+import { type ChangeEvent, useEffect, useState } from 'react'
+import type { CompaniesPayload } from './api/type.ts'
+import { dateFormat } from '../../shared/lib/date/dateFormat.ts'
 
 export const Organizations = () => {
   const { data } = useGetCompaniesQuery({ id: '12' })
   const header = data?.name
+  const [updateCompany] = useUpdateCompaniesMutation()
+
+  const [formData, setFormData] = useState<CompaniesPayload>({
+    name: '',
+    shortName: '',
+    businessEntity: '',
+    contract: {
+      no: '',
+      issue_date: '',
+    },
+    type: [],
+  })
+
+  useEffect(() => {
+    if (data) {
+      setFormData({
+        name: data.name,
+        shortName: data.shortName,
+        businessEntity: data.businessEntity,
+        contract: {
+          no: data.contract.no,
+          issue_date: dateFormat(data?.contract.issue_date.slice(0, 10)),
+        },
+        type: [...data.type],
+      })
+    }
+  }, [data])
+
+  const handleSubmit = async () => {
+    console.log('Sending updates:', formData)
+    try {
+      await updateCompany({ id: '12', updates: formData }).unwrap()
+      console.log(formData)
+    } catch (error) {
+      console.error('Полная ошибка:', error)
+    }
+  }
+
+  const handleContractChange = (e: ChangeEvent<HTMLInputElement>, name: string) => {
+    const { value } = e.currentTarget
+    setFormData((prev) => ({
+      ...prev,
+      contract: {
+        ...prev.contract,
+        [name]: value,
+      },
+    }))
+  }
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
@@ -28,24 +81,32 @@ export const Organizations = () => {
           <CustomIconButton color={'primary'} icon={trash} ariaLabel={'trash'} />
         </div>
       </div>
-      <Card title={'Company Details'} buttonIcon={edit} buttonText={'Edit'}>
-        <CardsList>
-          <AgreementLI />
-          <BusinessLI />
-          <CompanyLI />
-        </CardsList>
+      <Card>
+        <HeaderCompanyBlock submit={handleSubmit} />
+        <CardsBody>
+          <AgreementLI
+            key={1}
+            onChange={handleContractChange}
+            dateValue={formData.contract.issue_date}
+            noValue={formData.contract.no}
+          />
+          <BusinessLI key={2} />
+          <CompanyLI key={3} />
+        </CardsBody>
       </Card>
-      <Card title={'Contacts'} buttonIcon={edit} buttonText={'Edit'}>
-        <CardsList>
+      <Card>
+        <HeaderContactsBlock />
+        <CardsBody>
           <ResponsibleLI />
           <PhoneLI />
           <EmailLI />
-        </CardsList>
+        </CardsBody>
       </Card>
-      <Card title={'Photos'} buttonIcon={add} buttonText={'Add'}>
-        <CardsList>
+      <Card>
+        <HeaderPhotosBlock />
+        <CardsBody>
           <PhotosLi />
-        </CardsList>
+        </CardsBody>
       </Card>
     </Box>
   )
